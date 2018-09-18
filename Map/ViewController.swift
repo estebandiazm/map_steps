2d466ae27849fa6a3d9b6b62b74d0c883443238b
Juan Esteban Díaz <jdiaz@easysol.net>
2018-09-18 14:23:33 -0500

    Add center zoom and change map type

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -15,12 +15,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var map: MKMapView!
     private let manager = CLLocationManager()
     
+    @IBOutlet weak var standarItem: UITabBarItem!
+    
+    private var oldLocation : CLLocation!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
+        
 
     }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -38,12 +43,49 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         point.latitude = manager.location!.coordinate.latitude
         point.longitude = manager.location!.coordinate.longitude
         
-        let pin = MKPointAnnotation()
-        pin.title = "Punto"
-        pin.subtitle = "Pase por aquí"
-        pin.coordinate = point
+
+        if (nil == oldLocation){
+            oldLocation = locations[0]
+        }
         
-        map.addAnnotation(pin)
+        let distance : CLLocationDistance = manager.location!.distance(from: oldLocation)
+        print("Nueva ubicaicon \(oldLocation) Distancia: \(distance)")
+        if (distance > 50){
+        
+            let pin = MKPointAnnotation()
+            pin.title = "Punto"
+            pin.subtitle = "Pase por aquí"
+            pin.coordinate = point
+            
+            map.addAnnotation(pin)
+            
+            map.centerCoordinate = point
+            
+            let latDelta:CLLocationDegrees = 0.05
+            let lonDelta:CLLocationDegrees = 0.05
+            
+            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+            
+            let region = MKCoordinateRegion(center: point, span: span)
+            map.setRegion(region, animated: true)
+            
+            map.showsScale = true
+            
+            oldLocation = locations[0]
+        }
+        
+    }
+    
+
+    @IBAction func satelliteAction(_ sender: Any) {
+        map.mapType = MKMapType.satellite
+    }
+
+    @IBAction func mapStandardAction(_ sender: Any) {
+         map.mapType = MKMapType.standard
+    }
+    @IBAction func mapHybridAction(_ sender: Any) {
+         map.mapType = MKMapType.hybrid
     }
 }
 
