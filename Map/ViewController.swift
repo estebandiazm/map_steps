1bbf1ce89fb3e0974065319f4631638547b71e06
Juan Esteban Díaz <jdiaz@easysol.net>
2018-09-18 11:10:53 -0500

    upload project

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    private let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+
     }
-
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            manager.startUpdatingLocation()
+            map.showsUserLocation = true
+        } else {
+            manager.stopUpdatingLocation()
+            map.showsUserLocation = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var point = CLLocationCoordinate2D()
+        point.latitude = manager.location!.coordinate.latitude
+        point.longitude = manager.location!.coordinate.longitude
+        
+        let pin = MKPointAnnotation()
+        pin.title = "Punto"
+        pin.subtitle = "Pase por aquí"
+        pin.coordinate = point
+        
+        map.addAnnotation(pin)
+    }
 }
 
